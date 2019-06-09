7edb0b2bb23cda5818af6d9e70081ebfa6ed15bb
tbeleyur <tbeleyur@orn.mpg.de>
2019-06-09 20:12:24 +0200

    try to make the simulations parallel processing safe

--- simulations/effect_of_group_size/CPN_effect_of_Group_Size.py
+++ simulations/effect_of_group_size/CPN_effect_of_Group_Size.py
@@ -6,9 +6,11 @@ Created on Sat Jun 08 16:59:26 2019
 
 @author: tbeleyur
 """
+import hashlib
 import multiprocessing as mp
 from multiprocessing import Pool
 import time
+import uuid
 import os
 import sys
 sys.path.append('..//..//poisson-disc-master//')
@@ -33,21 +35,28 @@ common_kwargs.keys()
 common_kwargs['call_directionality'] = call_directionality_fn
 common_kwargs['hearing_directionality'] = hearing_directionality_fn
 
-def run_each_groupsize(group_size,  num_replicates = 10, kwargs=common_kwargs):
+def run_each_groupsize(group_size,  num_replicates = 1, kwargs=common_kwargs):
     '''
     
+    thanks to Raymond Hettinger for the integer hashing comment
+    https://stackoverflow.com/a/16008760/4955732
     '''
     start = time.time()
-
+    # a little bit of circus to ensure this code can still be run parallely 
+    # and not result in repeated seeds!! 
+    file_uuid = str(uuid.uuid4())
+    unique_seed = int(hashlib.sha1(file_uuid).hexdigest(), 16) % (10 ** 8)
+    np.random.seed(unique_seed)
+    unique_name = 'uuid_'+file_uuid+'_numpyseed_'+str(unique_seed)
     simoutput_container = {(group_size,i) : None for i in range(num_replicates)}
-    
+
     # set the varying factor - group size 
     common_kwargs['Nbats'] = group_size
     print('RUNNING '+str(group_size)+' bat simulations now')
     for replicate_run in xrange(num_replicates):
         num_echoes, sim_output = run_CPN(**kwargs)
         simoutput_container[(group_size, replicate_run)] = sim_output
-    picklefilename = 'results//' +'group_size_effect_'+str(group_size)+'bats_CPN.pkl'
+    picklefilename = 'results//'+str(group_size)+'bats_CPN_'+unique_name#+'_.pkl'
     print('did ' + str(group_size)+ ' in ' + str(time.time()-start))
     try:
         with open(picklefilename, 'wb') as picklefile:
@@ -65,9 +74,12 @@ def wrapper_each_group_size(groupsize):
 
 
 # run simulations for all group sizes of interest
-group_sizes = [5,10,15,20,25]
+group_sizes = [5,5]
 start = time.time()
+# pool = Pool(4)
+# all_outputs = pool.map(wrapper_each_group_size, group_sizes)
 all_outputs = map(wrapper_each_group_size, group_sizes)
 print('OVERALL SIMS TOOK', time.time()-start )
 
-    
+
+# check 

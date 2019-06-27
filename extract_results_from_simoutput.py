2cd18a4a72c7a3be87ac05ab399e118db515aaaf
tbeleyur <tbeleyur@orn.mpg.de>
2019-06-27 15:51:22 +0200

    latest version of extract results - no major changes

--- extract_results_from_simoutput.py
+++ extract_results_from_simoutput.py
@@ -4,9 +4,17 @@ Created on Sat Jun 08 10:29:30 2019
 
 @author: tbeleyur
 """
-
+import pickle
 import numpy as np
 import scipy.stats 
+
+import re
+import glob
+import multiprocessing
+import sys
+
+import matplotlib.pyplot as plt 
+plt.rcParams['agg.path.chunksize'] = 1000
 
 
 def gather_results_from_simoutput(sim_output, detected_status=1):
@@ -109,4 +117,71 @@ def get_heard_NN(echo_ids, sounds_in_ipi, detected_status=1):
     rank_distances = scipy.stats.rankdata(distance_conspecifics)
     heard_neigbours_rank = rank_distances[echo_ids==detected_status]
     return(heard_neigbours_rank)
+
+
+def get_summary_for_each_variablevalue(variable_value,all_files, **kwargs):
+    '''relies on a file called result_files 
+
+    Parameters
+    ----------
+    variable_value : variable value
+
+    all_files : list with filepaths. Each filepath is of a pickle file that has
+                multiple simulation runs in it. 
+
+    Returns
+    -------
+    
+    '''
+    print(variable_value, ' starting')
+    group_results = []
+    numechoes_heard = []
+    for each_simrun in all_files[variable_value]:
+        with open(each_simrun, 'rb') as results:
+            simoutputs = pickle.load(results)
+            for _, outputs in simoutputs.iteritems():
+                processed_results = gather_results_from_simoutput(outputs,
+                                                                      kwargs['detected_status'])
+                num_echoes = processed_results.shape[0]
+                group_results.append(processed_results)
+                numechoes_heard.append(num_echoes)
+    r, theta, nn, level = np.split(np.concatenate(group_results).reshape(-1,4), 4, axis=1)
+    print(variable_value, 'group size Done')
+    return(numechoes_heard, r, theta, nn, level)
+
+def process_simulation_outputs(results_folder, variable_values, **kwargs):
+    '''
+    '''
+    result_files = {value: glob.glob(results_folder+str(value)+'bats_CPN*.pkl') for value in variable_values}
+    if len(kwargs.keys())>0:
+        summary_for_variablevalue = lambda  X: get_summary_for_each_variablevalue( X, result_files,**kwargs)
+    else:
+        summary_for_variablevalue = lambda  X: get_summary_for_each_variablevalue( X, result_files)
+
+    outputs = map(summary_for_variablevalue, variable_values)
+    numechoes, r, theta, nn, level = {}, {}, {}, {}, {}
+
+    for value_output, value in zip(outputs, variable_values):
+        numechoes[value] = value_output[0]
+        r[value] = value_output[1]
+        theta[value] = value_output[2]
+        nn[value] = value_output[3]
+        level[value] = value_output[4]
+    return(numechoes, r, theta, nn, level)
+        
+        
+        
+        
+
+
+if __name__ == '__main__':
+    results_folder = 'D:\\the_cocktail_party_nightmare\\simulations\\effect_of_group_size_noshadowing\\'
+    group_size = [5]
+    result_files = {Nbats : glob.glob(results_folder+str(Nbats)+'bats_CPN*.pkl') for Nbats in group_size}
+
+    nechoes, r, theta, nn, level = process_simulation_outputs(results_folder,[20],
+                                                              **{'detected_status':1})
+
+    nechoes, r_not, theta_not, nn_not, level_not = process_simulation_outputs(results_folder,[20],
+                                                              **{'detected_status':0})
 

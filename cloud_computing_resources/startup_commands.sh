58e8f5d8725f9415cce255ab796f71f6f7721a78
tbeleyur <tbeleyur@orn.mpg.de>
2019-07-10 15:14:37 +0200

    add sudo before the bash!

--- cloud_computing_resources/startup_commands.sh
+++ cloud_computing_resources/startup_commands.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y htop git python-pip
 git clone https://github.com/thejasvibr/the_cocktail_party_nightmare.git
 cd the_cocktail_party_nightmare/
 git checkout dev_submission2 
-bash cloud_computing_resources/run_script.sh
+sudo bash cloud_computing_resources/run_script.sh

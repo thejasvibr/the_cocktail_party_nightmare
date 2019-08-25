74e999ab72ad96e84896baac3b1757c7630c8b36
thejasvibr <tbeleyur@orn.mpg.de>
2019-08-25 13:04:39 +0200

    try to fix the BUG which causes a irrelevant Nbats to appear in the simulation identifiers -- DO NOT USE THIS. The simulation output itself matches the proper Nbats  - and so this is good news!

--- simulations/create_and_save_common_simparameters.py
+++ simulations/create_and_save_common_simparameters.py
@@ -15,35 +15,37 @@ import statsmodels.api as sm
 class parameter_container():
     
     def __init__(self):
-        self.load_parameters()
-        
+        self.bistatic_TS_file = '..//data//bistatic_TS_bat.csv'
+        self.shadowing_model_file = '..//data//acoustic_shadowing_model.pkl'
+        self.tempmasking_file = '..//data//temporal_masking_function.pkl'
+        self.spatial_unmasking_file = '..//data//spatial_release_fn.csv'
+
         
     def load_parameters(self):
         self.kwargs={}
         self.kwargs['interpulse_interval'] = 0.1
         self.kwargs['v_sound'] = 330.0
         self.kwargs['simtime_resolution'] = 10**-6
-        self.kwargs['echocall_duration'] = 0.0025
-        self.kwargs['call_directionality'] = None
-        self.kwargs['hearing_directionality'] = None
-        reflection_func = pd.read_csv('..//data//bistatic_TS_bat.csv')
+        self.kwargs['echocall_duration'] = 0.0025        
+        reflection_func = pd.read_csv(self.bistatic_TS_file)
         self.kwargs['reflection_function'] = reflection_func
         self.kwargs['heading_variation'] = 10.0
         self.kwargs['min_spacing'] = 0.5
-        self.kwargs['Nbats'] = 5
+        self.kwargs['Nbats'] = None
         self.kwargs['source_level'] = {'dBSPL' : 100, 'ref_distance':1.0}
         self.kwargs['hearing_threshold'] = 20
         self.kwargs['rectangle_width'] = 0.25
         self.kwargs['implement_shadowing'] = True
         
         # load the model to predict shadowing 
-        self.kwargs['acoustic_shadowing_model']=sm.load('..//data//acoustic_shadowing_model.pkl')
         
-        tempmasking_file = '..//data//temporal_masking_function.pkl'
-        with open(tempmasking_file, 'rb') as pklfile:
+        self.kwargs['acoustic_shadowing_model']=sm.load(self.shadowing_model_file)
+        
+        
+        with open(self.tempmasking_file, 'rb') as pklfile:
             temporal_masking_fn = pickle.load(pklfile)
-            
-        spatial_unmasking_fn = pd.read_csv('..//data//spatial_release_fn.csv')
+        
+        spatial_unmasking_fn = pd.read_csv(self.spatial_unmasking_file)
         
         self.kwargs['temporal_masking_thresholds'] = temporal_masking_fn
         self.kwargs['spatial_release_fn'] = np.array(spatial_unmasking_fn)[:,1:]
@@ -53,6 +55,9 @@ class parameter_container():
         self.kwargs['hearing_directionality'] = lambda X, B=2 : B*(np.cos(np.deg2rad(X))-1)
 
 
+common_params_class = 'commonsim_params_class.pkl'
+with open(common_params_class,'wb') as paramsclass:
+    pickle.dump(parameter_container, paramsclass)
 
 
 common_params_file = 'commonsim_params_v11.pkl'
--- simulations/effect_of_group_size/make_params_groupsize.py
+++ simulations/effect_of_group_size/make_params_groupsize.py
@@ -7,17 +7,37 @@ Created on Sat Aug 24 22:10:10 2019
 @author: tbeleyur
 """
 import dill as pickle
+import pandas  as pd
+import numpy as np 
+import statsmodels.api as sm
 
-with open('../commonsim_params_v11.pkl', 'rb') as pkl:
-    current_sim_params = pickle.load(pkl)
+with open('../commonsim_params_class.pkl','rb') as pklfile:
+    simparams_class = pickle.load(pklfile)
 
-group_sizes = [5, 10, 20, 50, 100, 200]
+group_sizes = [5, 10, 20, 50]
+
 
 for groupsize in group_sizes:
-    current_sim_params.kwargs['Nbats']  = groupsize
-    current_sim_params.kwargs['Nruns']  = 1000
-    current_sim_params.kwargs['detailed description'] = '1000 runs across'+str(groupsize)
+        
+    groupsize_params = simparams_class()
+    groupsize_params.bistatic_TS_file = '..//..//data//bistatic_TS_bat.csv'
+    groupsize_params.shadowing_model_file = '..//..//data//acoustic_shadowing_model.pkl'
+    groupsize_params.tempmasking_file = '..//..//data//temporal_masking_function.pkl'
+    groupsize_params.spatial_unmasking_file = '..//..//data//spatial_release_fn.csv'
+    
+    groupsize_params.load_parameters()
+    
+    
+    groupsize_params.kwargs['Nbats']  = groupsize
+    groupsize_params.kwargs['Nruns']  = 10
+    groupsize_params.kwargs['detailed description'] = '1000 runs across'+str(groupsize)
     
     param_filename = 'groupsize_simparams_' + str(groupsize)+'_.paramset'
     with open(param_filename,'wb') as pklfile:
-        pickle.dump(current_sim_params, pklfile)
+        pickle.dump(groupsize_params, pklfile)
+    del groupsize_params
+
+
+
+# load a file and check what the Nbstas is :
+        

9d1c893765512e4c8bc89602d627dadc0d13ce52
thejasvibr <tbeleyur@orn.mpg.de>
2019-09-04 14:33:29 +0200

    add proper __init__ file for bridson

--- CPN/__init__.py
+++ CPN/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
 
-from bridson.bridson import poisson_disc_samples
+from bridson import poisson_disc_samples
 from acoustics.detailed_sound_propagation import soundprop_w_acoustic_shadowing, calc_RL
 

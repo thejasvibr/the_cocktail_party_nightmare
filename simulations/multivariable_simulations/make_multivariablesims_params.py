c56ea017482845dfdf44b73031be6366ce602b38
thejasvibr <tbeleyur@orn.mpg.de>
2019-09-04 17:06:25 +0200

    estimate time taken for 200 bats at 200 runs for all param combinations

--- simulations/multivariable_simulations/make_multivariablesims_params.py
+++ simulations/multivariable_simulations/make_multivariablesims_params.py
@@ -22,19 +22,19 @@ import statsmodels.api as sm
 
 join_into_string = lambda Y: '*'.join(map(lambda X:str(X), Y))
 
-group_size = 50
+group_size = 200
 interpulse_duration = [0.05, 0.1]
 call_duration = [0.001, 0.0025]
 shadowing = [True, False]
 source_level = [94, 100, 106]
 spacing = [0.5, 1.0]
 group_heading_variation = [10, 90]
-atmospheric_absorption = [-1.0,-1.5,-2.0]
-number_of_simulation_runs = 1 
+atmospheric_absorption = [0]
+number_of_simulation_runs = 200 
 
 with open('../common_simulation_parameters.paramset','rb') as pklfile:
     simulation_parameters = dill.load(pklfile)
-
+i = 0
 for ipi in interpulse_duration:
     for call_durn in call_duration:
         for w_shadowing in shadowing:
@@ -42,6 +42,7 @@ for ipi in interpulse_duration:
                 for interbat_spacing in spacing:
                     for heading_variation in group_heading_variation:
                         for atm_abs in atmospheric_absorption:
+                            i += 1
                             simulation_parameters['Nbats']  = group_size
                             simulation_parameters['Nruns']  = number_of_simulation_runs
                             description = str(simulation_parameters['Nruns']) + 'runs across'+str(group_size)
@@ -61,19 +62,13 @@ for ipi in interpulse_duration:
                             param_filename = 'simulation_parameters_' + variables_as_string+'_.paramset'
                             with open(param_filename,'wb') as pklfile:
                                 dill.dump(simulation_parameters, pklfile)
-    
 
-
-all_params = glob.glob('*.paramset')
-chosen = np.random.choice(all_params, 5)
-
-for each in all_params:
-    if not each in chosen:
-        os.remove(each)
 #
-results = glob.glob('*.simresults')
-all_res = []
-for each in results:
-    with open(each, 'rb') as res:
-        all_res.append(dill.load(res))
-        
+#all_params = glob.glob('*.paramset')
+#chosen = np.random.choice(all_params, 4)
+#
+#
+#for each in all_params:
+#    if not each in chosen:
+#        os.remove(each)
+
